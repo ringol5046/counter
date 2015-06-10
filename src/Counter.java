ca8d3db0f7bebde661c3beab916a46bdf2f07a93
ringol5046 <ringol5046@gmail.com>
2015-06-10 16:48:19 -0400

    checked

--- src/Counter.java
+++ src/Counter.java
@@ -4,13 +4,19 @@ import java.util.Scanner;
 
 /*
  *
- Define a class called Counter. An object of this class is used to count thing,
- so it records a count that is a nonnegative whole number. include method to set
- the counter to a given integer, to increase the count by 1, and to decrease the
- count by 1. be sure that no method allows the value of the counter to become
- negative. also include a method that returns the current count value as an integer
- a method toString that return the current count value as string suitable for display
- on the screen, and a method that tests whether the current count value is zero.
+ Define a class called Counter:
+ An object of this class is used to count thing, it records a count that is a nonnegative whole number.
+ For the counter class, you need to have:
+ 
+ -a method to set the counter to a given integer
+ -a method to increase the count by 1
+ -a method to decrease the count by 1.
+ -a method that returns the current count value as an integer
+ -a method toString that return the current count value as string suitable for display on the screen
+ -a method that tests whether the current count value is zero.
+ 
+ be sure that no method allows the value of the counter to become negative.
+ 
  write a program to test your class definition.
  */
 
@@ -24,9 +30,8 @@ class Counter {
     private int number;
     private int total;
     private int count;
-    boolean False;
     
-    public Counter (){ 
+    public Counter (){
     }
     
     public Counter(int n){
@@ -40,7 +45,7 @@ class Counter {
     }
     
     public void setCounter(int n){
-        setFirst(n);
+        setTotal(n);
     }
     
     public void decrease(){
@@ -54,17 +59,17 @@ class Counter {
         }
     }
     
-    public String getTotal(int total){
-        return Integer.toString(total);
+    public String getCount(){
+        return Integer.toString(count);
     }
-
-    private void setFirst(int n) {
+    
+    private void setTotal(int n) {
         total=n;
     }
- 
+    
     public boolean test(int count){
         if(count!=0){
-            return False;
+            return false;
         }
         else{
             return true;
