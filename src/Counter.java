7891cdf8f30adbcaf917649f785abc71f048cc7b
ringol5046 <ringol5046@gmail.com>
2015-06-10 15:08:59 -0400

    delete
    delete auto generating file move description to counter class

--- src/Counter.java
+++ src/Counter.java
@@ -3,10 +3,17 @@ import static java.lang.System.in;
 import java.util.Scanner;
 
 /*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
+ *
+ Define a class called Counter. An object of this class is used to count thing,
+ so it records a count that is a nonnegative whole number. include method to set
+ the counter to a given integer, to increase the count by 1, and to decrease the
+ count by 1. be sure that no method allows the value of the counter to become
+ negative. also include a method that returns the current count value as an integer
+ a method toString that return the current count value as string suitable for display
+ on the screen, and a method that tests whether the current count value is zero.
+ write a program to test your class definition.
  */
+
 
 /**
  *
